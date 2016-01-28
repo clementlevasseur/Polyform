a7c2f54ae3e01f1fb93c7926615b2160646d7953
Clément Levasseur <clementlevasseur@gmail.com>
2016-01-28 19:56:17 +0100

    random triangle

--- js/polyform.js
+++ js/polyform.js
@@ -1,41 +1,67 @@
 var BABYLON = require("BABYLONJS");
 
-Polyform = (function () {
+Polyform = (function() {
 
-   var pf = function () {};
+    var pf = function() {};
 
-   pf.prototype.init = function (canvas) {
-      this.engine = new BABYLON.Engine(canvas, true);
+    pf.prototype.init = function(canvas) {
 
-      this.scene = new BABYLON.Scene(this.engine);
-      this.scene.clearColor = new BABYLON.Color3(0, 1, 0);
+        this.nbTriangle = 20;
+        this.area = new BABYLON.Vector3(10, 10, 10);
+        this.points = [];
 
-      var camera = new BABYLON.ArcRotateCamera("camera1", 0.1, 0.1, 10, new BABYLON.Vector3(0, 5, -10), this.scene);
-      camera.setTarget(BABYLON.Vector3.Zero());
-      camera.attachControl(canvas, false);
+        this.engine = new BABYLON.Engine(canvas, true);
 
-      var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
-      light.intensity = .5;
+        this.scene = new BABYLON.Scene(this.engine);
+        this.scene.clearColor = new BABYLON.Color3(0, 1, 0);
 
-      var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, this.scene);
-      sphere.position.y = 1;
+        var camera = new BABYLON.ArcRotateCamera("camera1", 0.1, 0.1, 10, new BABYLON.Vector3(0, 5, -10), this.scene);
+        camera.setTarget(BABYLON.Vector3.Zero());
+        camera.attachControl(canvas, false);
 
-      var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, this.scene);
-   };
+        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
+        light.intensity = .5;
+
+        var x, y, z;
+        for (var i = 0; i < this.nbTriangle*3; i++) {
+            var position = this.area.clone();
+
+            position.x *= Math.random() - 0.5;
+            position.y *= Math.random() - 0.5;
+            position.z *= Math.random() - 0.5;
+
+            this.points.push(position);
+        }
+
+        var vertices = [];
+        var indices = [];
+        for (var i = 0; i < this.points.length; i++) {
+            vertices.push(this.points[i].x, this.points[i].y, this.points[i].z);
+            indices.push(i);
+        }   
+
+        console.log(this.points);
+        this.vd = new BABYLON.VertexData();
+        this.vd.set(vertices, BABYLON.VertexBuffer.PositionKind);
+
+        this.mesh = new BABYLON.Mesh('main', this.scene);
+        this.vd.applyToMesh(this.mesh);
+        this.mesh.setIndices(indices);
+    };
 
 
-   pf.prototype.render = function () {
+    pf.prototype.render = function() {
 
-      // Register a render loop to repeatedly render the scene
-      this.engine.runRenderLoop(function () {
-         this.scene.render();
-      }.bind(this));
-      // Watch for browser/canvas resize events
-      window.addEventListener("resize", function () {
-         this.engine.resize();
-      });
-      
-   };
+        // Register a render loop to repeatedly render the scene
+        this.engine.runRenderLoop(function() {
+            this.scene.render();
+        }.bind(this));
+        // Watch for browser/canvas resize events
+        window.addEventListener("resize", function() {
+            this.engine.resize();
+        }.bind(this));
 
-   return pf;
+    };
+
+    return pf;
 })();
